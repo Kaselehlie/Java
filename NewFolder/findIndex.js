3e2e5bdaa2f2afe85e3916dbea2bc5734252df18
Sharon Gonzalez <sharongnzlz@gmail.com>
2025-12-03 19:35:51 -0800

    findIndex-forgot to click save b4 commiting

--- NewFolder/findIndex.js
+++ NewFolder/findIndex.js
@@ -1,37 +1,36 @@
-
 //.......TASK..............
 // Use the `find` method to locate the first creature of the "Water" type and log its name to the console.
 // Use the `findIndex` method to locate the index of the "Griffin" in the mythical creatures array and log it to the console.
 // Use the `find` method to locate the first creature last seen in "Enchanted Forest".
 
 const mythicalCreatures = [
-	{name: "Dragon", type: "Fire", lastSeen: "Volcano Valley"},
-	{name: "Mermaid", type: "Water", lastSeen: "Coral Caves"},
-	{name: "Unicorn", type: "Land", lastSeen: "Enchanted Forest"},
-	{name: "Griffin", type: "Air", lastSeen: "Highwind Mountains"},
-	{name: "Kraken", type: "Water", lastSeen: "Abyssal Depths"}
+  { name: "Dragon", type: "Fire", lastSeen: "Volcano Valley" },
+  { name: "Mermaid", type: "Water", lastSeen: "Coral Caves" },
+  { name: "Unicorn", type: "Land", lastSeen: "Enchanted Forest" },
+  { name: "Griffin", type: "Air", lastSeen: "Highwind Mountains" },
+  { name: "Kraken", type: "Water", lastSeen: "Abyssal Depths" },
 ];
 //.............springboard solution......................with my comments.................
-const firstWaterCreature = mythicalCreatures.find(//instructing to locate with 'find' method
-  	function (creature)
- 	{
- 		return creature.type === "Water";// creature type
- 	}
- );
- console.log(firstWaterCreature.name);// console printed Mermaid
+const firstWaterCreature = mythicalCreatures.find(
+  //instructing to locate with 'find' method
+  function (creature) {
+    return creature.type === "Water"; // creature type
+  }
+);
+console.log(firstWaterCreature.name); // console printed Mermaid
 //-------------------------------------------------------------------------------------------------
-const griffinIndex = mythicalCreatures.findIndex(// 'findIndex' method to find index
- 	function (creature)
- 	{
- 		return creature.name === "Griffin";// creature name 
- 	}
- );
+const griffinIndex = mythicalCreatures.findIndex(
+  // 'findIndex' method to find index
+  function (creature) {
+    return creature.name === "Griffin"; // creature name
+  }
+);
 console.log(griffinIndex); //console printed 3
 //.........................................................................................................
- const enchantedForestCreature = mythicalCreatures.find(// 'find' method
- 	function (creature)
- 	{
- 		return creature.lastSeen === "Enchanted Forest";// creature lastSeen
- 	}
- );
- console.log(enchantedForestCreature.name); //console printed Unicorn
+const enchantedForestCreature = mythicalCreatures.find(
+  // 'find' method
+  function (creature) {
+    return creature.lastSeen === "Enchanted Forest"; // creature lastSeen
+  }
+);
+console.log(enchantedForestCreature.name); //console printed Unicorn
