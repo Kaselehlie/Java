5274025a75e4e4b747858d8515092d8a60991761
Sharon Gonzalez <sharongnzlz@gmail.com>
2025-12-06 18:27:23 -0800

    hoistingExercise

---
+++ NewFolder/hoistingExercise.js
@@ -0,0 +1,29 @@
+//Task 1: Declare a Destination Variable */
+// TODO: Use `let` to declare a variable named `destination` and assign it the value `"Ancient Egypt"`. Print the destination to the console.
+let destination = "Ancient Egypt";
+console.log(destination);
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+//Task 2: Change the Destination
+//TODO: Now, change the `destination` variable to `"Medieval Europe"`. Print the new destination to the console.
+
+destination = "Medieval Europe";
+console.log(destination);
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+//Task 3: Declare a Constant Travel Date
+//TODO: Use `const` to declare a variable named `travelDate` and set it to `"2024-03-15"`. Try to change the `travelDate` to another value and observe and explain what happens as a comment.
+
+const travelDate = "2024-03-15";
+//travelDate = "2024-04-20";
+
+//Observations:
+//TODO: Explain here:----->when I tried to change the value of travelDate, console printed TypeError because constants cannot be reassigned
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+//Task 4: Experiment with Variable Hoisting
+//TODO: Before declaring any variable, try to print a variable named `timeMachineModel` to the console. Then, declare `timeMachineModel` using `var` and assign it the value `"T-800"`. Observe and explain what happens as a comment.
+
+var timeMachineModel = "T-800";
+//Observations:
+// TODO: Explain here:----->When I tried to print the variable before declaring it, console printed 'undefined'. After I declared it using "var" and assigned its value, then it worked. Variables declared with 'var' are hoisted to the top of their scope but are not initialized until the declaration is evaluated.
